bb5b7ab42204e88b87c40670cc5242eaf07d58df
Diego Rubí Salas <diegoalru@gmail.com>
2023-06-14 22:16:14 -0600

    DetailActivity eliminada del proyecto.

--- app/src/main/java/com/example/wordsapp/LetterAdapter.kt
+++ app/src/main/java/com/example/wordsapp/LetterAdapter.kt
@@ -67,7 +67,7 @@ class LetterAdapter :
         holder.button.setOnClickListener {
             val context = holder.view.context
 
-            val intent = Intent(context, DetailActivity::class.java)
+            val intent = Intent(context, WordListFragment::class.java)
             intent.putExtra(WordListFragment.LETTER, holder.button.text.toString())
             context.startActivity(intent)
         }
